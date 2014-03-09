f337754be0480ba0b223ec58b5ee13211d5f7b43
Pamela Gunawan <pamela.gunawan@yahoo.com>
2014-03-09 14:06:28 -0700

    Fixed bugs

--- src/Chat.java
+++ src/Chat.java
@@ -1,17 +1,9 @@
 import java.io.*;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.*;
 
-
-
 public class Chat implements ActionListener{
-	
-    
-    
 	//Declare JFrame components
 	private JTextArea display;
 	private JTextField Jinput;
@@ -20,8 +12,6 @@ public class Chat implements ActionListener{
 	private JFrame frame;
 	
     public Chat(){
-    	
-    	
     	//Create JFrame
     	frame = new JFrame("ChatVader");
     	frame.setSize(700,500);
@@ -31,6 +21,7 @@ public class Chat implements ActionListener{
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	
     	//Initialize JTextArea
+    	display = new JTextArea("Welcome to ChatVader (type 'Bye' to quit)");
     	display = new JTextArea(25,57);
     	//Set JTextArea so it cannot be edited
     	display.setEditable(false);
@@ -42,6 +33,12 @@ public class Chat implements ActionListener{
     	
     	//Initialize JTextField
     	Jinput = new JTextField(50);
+    	Jinput.setText("");
+    	Jinput.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                actionEnter(evt);
+            }
+        });
     	
     	//Initialize JButton
     	btn = new JButton("Send");
@@ -52,67 +49,30 @@ public class Chat implements ActionListener{
     	frame.getContentPane().add(Jinput);
     	frame.getContentPane().add(btn);
     	
-    	
-    	
     	//Show the JFrame
     	frame.setVisible(true);
-    	
     }
-
+    
 	public static void main(String[]args)throws IOException{
-		 SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                new Chat();
-	            }
-	        });
- 
-        
-		/*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = "";
-        
-		do{
-			//Adds some delay to make the chatbot seem more realistic
-			try {
-				Thread.sleep(750);
-			}
-			catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-            
-			System.out.print("<User>");
-			input = br.readLine();
-			
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Chat();
+            }
+        });
+	}
+	
+	@Override
+	public void actionPerformed (ActionEvent ae) {
+		//If user clicks the send button
+		if(ae.getActionCommand().equals("Send")){
+			//Set input to JTextField's value
+			String input = Jinput.getText();
 			//Edit input to slash any special characters, leading/trailing spaces, etc.
 			input=input.trim();
 			input=input.toLowerCase();
-            
-			//Adds some delay to make the chatbot seem more realistic
-			try {
-				Thread.sleep(500);
-			}
-			catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-            
-			System.out.println("<Vader>"+Bot.Bot(input));
-		}while (!input.contains("bye"));*/
-        
-	}
-
-
-	
-	@Override
-	public void actionPerformed(ActionEvent ae) {
-		System.out.println("Welcome to ChatVader (enter 'Bye' to quit the program)");
-
-		//If user clicks the send button
-		if(ae.getActionCommand().equals("Send")){
 			
-			//Set input to JTextField's value
-			String input = Jinput.getText();
 			String output = Bot.Bot(input);
+			Jinput.setText("");
 			
 			try {
 				Thread.sleep(750);
@@ -130,10 +90,35 @@ public class Chat implements ActionListener{
 				e1.printStackTrace();
 			}
 			display.setText(display.getText() + "<Vader> " +output+ "\n");
-			
-			
 		}
-		
 	}
-
+	
+	public void actionEnter (KeyEvent ke) {
+		//If user clicks the send button
+		if(	ke.getKeyCode()==KeyEvent.VK_ENTER){
+			//Set input to JTextField's value
+			String input = Jinput.getText();
+			//Edit input to slash any special characters, leading/trailing spaces, etc.
+			input=input.trim();
+			input=input.toLowerCase();
+			
+			String output = Bot.Bot(input);
+			Jinput.setText("");
+			
+			try {
+				Thread.sleep(750);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			display.setText(display.getText() + "<User> " +input+ "\n");
+			try {
+				Thread.sleep(750);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			display.setText(display.getText() + "<Vader> " +output+ "\n");
+		}
+	}
 }
