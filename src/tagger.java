533ae739f7c1c8c4fc91d74ceda3b9e4403c9d80
Pamela Gunawan <pamela.gunawan@yahoo.com>
2014-03-13 08:20:10 -0700

    implementing nlp pt. 2

---
+++ src/tagger.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+public class tagger{
+
+	public static String tag(String input) throws IOException{
+
+		MaxentTagger tagger = null;
+		try {
+			tagger = new MaxentTagger("taggers/left3words-wsj-0-18.tagger");
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} catch (ClassNotFoundException e2) {
+			e2.printStackTrace();
+		}
+		String tagged = tagger.tagString(input);
+		String output = null;
+		try {
+			output = Bot.Bot(tagged);
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+		return tagged;
+	}
+}
